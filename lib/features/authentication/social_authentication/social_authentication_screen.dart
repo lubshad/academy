bf525d840a292be731f09093d8aa8c0f1e97e675
lubshad <lubshad4u4@gmail.com>
2024-02-15 12:14:35 +0530

    key added

--- lib/features/authentication/social_authentication/social_authentication_screen.dart
+++ lib/features/authentication/social_authentication/social_authentication_screen.dart
@@ -23,119 +23,121 @@ class _SocialAuthenticationScreenState extends State<SocialAuthenticationScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(paddingLarge),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(
-                      paddingLarge,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(paddingLarge),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        paddingLarge,
+                      ),
                     ),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: paddingLarge,
-                    vertical: paddingXL,
-                  ),
-                  child: Column(
-                    children: [
-                      SvgPicture.asset(
-                        Assets.svgs.study,
-                        width: 100,
-                      ),
-                      gapLarge,
-                      Text(
-                        "Log in",
-                        style: context.titleLarge,
-                      ),
-                      gapLarge,
-                      FormHeader(
-                        label: "Email",
-                        child: TextFormField(
-                          controller: emailController,
-                          validator: emailValidator,
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.email_outlined),
-                            hintText: "Email",
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: paddingLarge,
+                      vertical: paddingXL,
+                    ),
+                    child: Column(
+                      children: [
+                        SvgPicture.asset(
+                          Assets.svgs.study,
+                          width: 100,
+                        ),
+                        gapLarge,
+                        Text(
+                          "Log in",
+                          style: context.titleLarge,
+                        ),
+                        gapLarge,
+                        FormHeader(
+                          label: "Email",
+                          child: TextFormField(
+                            controller: emailController,
+                            validator: emailValidator,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.email_outlined),
+                              hintText: "Email",
+                            ),
                           ),
                         ),
-                      ),
-                      gapLarge,
-                      FormHeader(
-                        label: "Password",
-                        child: TextFormField(
-                          obscureText: !passwordVisible,
-                          validator: passwordValidator,
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                            errorText: passwordError,
-                              prefixIcon: const Icon(
-                                Icons.lock_outline,
-                              ),
-                              hintText: "Password",
-                              suffixIcon: IconButton(
-                                  onPressed: touglePasswordVisibility,
-                                  icon: Icon(visibilityIcon))),
+                        gapLarge,
+                        FormHeader(
+                          label: "Password",
+                          child: TextFormField(
+                            obscureText: !passwordVisible,
+                            validator: passwordValidator,
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                                errorText: passwordError,
+                                prefixIcon: const Icon(
+                                  Icons.lock_outline,
+                                ),
+                                hintText: "Password",
+                                suffixIcon: IconButton(
+                                    onPressed: touglePasswordVisibility,
+                                    icon: Icon(visibilityIcon))),
+                          ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: TextButton(
-                            onPressed: navigateForgotPassword,
-                            child: Text(
-                              "Forget Password?",
-                              style: context.labelLarge.copyWith(
-                                color: Colors.grey,
-                              ),
-                            )),
-                      ),
-                      gapLarge,
-                      LoadingButton(
-                          buttonLoading: loginButtonLoading,
-                          text: "Log In",
-                          onPressed: signInWithEmailAndPassword),
-                      gap,
-                      const Text("OR"),
-                      gap,
-                      LoadingButton(
-                        icon: SvgPicture.asset(
-                          Assets.svgs.icons8Google,
-                          height: paddingXL,
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: TextButton(
+                              onPressed: navigateForgotPassword,
+                              child: Text(
+                                "Forget Password?",
+                                style: context.labelLarge.copyWith(
+                                  color: Colors.grey,
+                                ),
+                              )),
                         ),
-                        backgroundColor: Colors.black,
-                        buttonLoading: buttonLoading,
-                        text: "Sign In With Google",
-                        onPressed: signInWithGoogle,
+                        gapLarge,
+                        LoadingButton(
+                            buttonLoading: loginButtonLoading,
+                            text: "Log In",
+                            onPressed: signInWithEmailAndPassword),
+                        gap,
+                        const Text("OR"),
+                        gap,
+                        LoadingButton(
+                          icon: SvgPicture.asset(
+                            Assets.svgs.icons8Google,
+                            height: paddingXL,
+                          ),
+                          backgroundColor: Colors.black,
+                          buttonLoading: buttonLoading,
+                          text: "Sign In With Google",
+                          onPressed: signInWithGoogle,
+                        ),
+                      ],
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Do not have an account?",
+                      style: context.labelLarge.copyWith(
+                        color: Colors.black.withOpacity(.5),
                       ),
-                    ],
-                  )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Do not have an account?",
-                    style: context.labelLarge.copyWith(
-                      color: Colors.black.withOpacity(.5),
                     ),
-                  ),
-                  // gapSmall,
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      // padding: EdgeInsets.zero,
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    // gapSmall,
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        // padding: EdgeInsets.zero,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                      onPressed: signupAction,
+                      child: Text(
+                        "Sign Up",
+                        style: context.bodyLarge,
+                      ),
                     ),
-                    onPressed: signupAction,
-                    child: Text(
-                      "Sign Up",
-                      style: context.bodyLarge,
-                    ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
